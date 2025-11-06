ed8f3e7ba19212847e674d3b94b6cb0ff11db2de
Maikon Sposito <mknsposito@gmail.com>
2025-11-06 14:50:22 -0300

    feat: metodos estaticos para calcular, aprendendo a utilizar asserts para tratar em metodos nao publicos e nao entradas de usuarios

--- src/main/java/br/com/maikonspo/oracle/semana1/calculadora/Calc.java
+++ src/main/java/br/com/maikonspo/oracle/semana1/calculadora/Calc.java
@@ -1,4 +1,25 @@
 package br.com.maikonspo.oracle.semana1.calculadora;
 
 public class Calc {
+
+    public static int soma(int a, int b){
+        int s = a + b;
+        return s;
+    }
+    public static long multiplica(long a, long b){
+        long s = a * b;
+        return s;
+    }
+    public static double divide(double a, double b) {
+        assert b != 0: "b não deverá ser zero.";
+        double s = a / b;
+        return s;
+    }
+
+    public static void main(String[] args) {
+        var a = 1;
+        var b = 0;
+        System.out.println(divide(a, b));
+
+    }
 }
