d2ea3937852db0873790e44609f1bd8de12a1a29
Maikon Sposito <mknsposito@gmail.com>
2025-11-06 10:03:40 -0300

    feat: criaçao do projeto dia 1, criar calculadora e tratar exceções

---
+++ src/main/java/br/com/maikonspo/oracle/Main.java
@@ -0,0 +1,6 @@
+package br.com.maikonspo.oracle;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/main/java/br/com/maikonspo/oracle/semana1/calculadora/Calc.java
@@ -0,0 +1,4 @@
+package br.com.maikonspo.oracle.semana1.calculadora;
+
+public class Calc {
+}
